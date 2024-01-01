937983090dedf9b53cd90b2575aca5519d341e08
Torh-21 <majarotoheeb@gmail.com>
2024-01-01 11:38:47 -0800

    PEP8 formatting fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello_flask():
     """Returns a string at the root route"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Returns a string at the hbnb route"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
